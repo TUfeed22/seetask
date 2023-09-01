6e156cf83acdbe2e1aaabd1393a273a260602923
v.krahmalev <v.krahmalev@itcomgk.ru>
2023-09-01 22:13:15 +0300

    Добавил страницу Мой профиль

--- src/Controller/User/UserController.php
+++ src/Controller/User/UserController.php
@@ -3,9 +3,12 @@
 namespace App\Controller\User;
 
 
+use App\Entity\User;
+use App\Form\EditUserFormType;
 use App\Service\RoleService;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -17,14 +20,19 @@ class UserController extends AbstractController
      * @throws Exception
      */
     #[Route('/user/profile', name: 'app_user_profile')]
-    public function profile(RoleService $roleService): Response
+    public function profile(Request $request, RoleService $roleService): Response
     {
         // получаем наименование ролей
         $roleNames = $roleService->getRoleName($this->getUser()->getRoles());
 
+        $user = new User();
+        $form = $this->createForm(EditUserFormType::class, $user);
+        $form->handleRequest($request);
+
         return $this->render('user/index.html.twig', [
             'title' => 'Мой профиль',
-            'role_names' => $roleNames
+            'role_names' => $roleNames,
+            'editUserForm' => $form->createView(),
         ]);
     }
 }
