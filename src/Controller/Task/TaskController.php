5967dd92d656920704345475534933fd84b3e541
v.krahmalev <v.krahmalev@itcomgk.ru>
2023-09-28 20:16:45 +0300

    Правки

--- src/Controller/Task/TaskController.php
+++ src/Controller/Task/TaskController.php
@@ -55,7 +55,7 @@ class TaskController extends BaseController
             // Добавить проект привязав к текущему пользователю
             $this->getCurrentUser()->addTask($task);
             // По умолчанию статус Новый
-            $task->setStatus(Status::New->value);
+            $task->setStatus(Status::new->value);
 
             $entityManager->persist($task);
             $entityManager->flush();
