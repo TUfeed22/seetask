491a2233948f7038c1306069e1eccab2b03552c7
v.krahmalev <v.krahmalev@itcomgk.ru>
2023-09-28 21:15:57 +0300

    Добавил компонент Статус

--- src/Enum/Status.php
+++ src/Enum/Status.php
@@ -24,14 +24,20 @@ enum Status: string
         return $options;
     }
 
+    /**
+     * Рендеринг статуса
+     *
+     * @param $status
+     * @return string
+     */
     public static function render($status): string
     {
         return match ($status) {
             Status::new->name => '<span class="badge bg-info" style="color: #000000">' . Status::new->value . '</span>',
             Status::work->name => '<span class="badge bg-primary" style="color: #000000">' . Status::work->value . '</span>',
             Status::suspended->name => '<span class="badge bg-warning" style="color: #000000">' . Status::suspended->value . '</span>',
-            Status::done->name => '<span class="badge bg-success" style="color: #090909">' . Status::done->value . '</span>',
-            Status::cancelled->name => '<span class="badge bg-danger" style="color: #050505">' . Status::cancelled->value . '</span>',
+            Status::done->name => '<span class="badge bg-success" style="color: #000000">' . Status::done->value . '</span>',
+            Status::cancelled->name => '<span class="badge bg-danger" style="color: #000000">' . Status::cancelled->value . '</span>',
             default => '<span class="badge bg-secondary" style="color: #000000">' . $status . '</span>',
         };
     }
